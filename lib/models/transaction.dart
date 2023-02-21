f5a2939ff64411d35431a79cc5b0eea1e723d6d2
samuelm00 <sa.mitterrutzner@gmail.com>
2023-02-21 09:43:07 +0100

    add simple transaction item stylings

---
+++ lib/models/transaction.dart
@@ -0,0 +1,18 @@
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transaction({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.date,
+  });
+
+  String get getId => id;
+  String get getTitle => title;
+  double get getAmount => amount;
+  DateTime get getDate => date;
+}
