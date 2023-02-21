efd885a7299f2f200377c9d0faa8f7e056556a00
samuelm00 <sa.mitterrutzner@gmail.com>
2023-02-21 15:42:09 +0100

    add simple test for chart_controller

---
+++ test/chart/controllers/chart_controller_test.dart
@@ -0,0 +1,70 @@
+import 'dart:math';
+
+import 'package:flutter_expense_tracker/features/chart/controllers/chart_controller.dart';
+import 'package:flutter_expense_tracker/models/transaction.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets(
+      'Check if transaction_controller_test groups transactions correctly multiple days',
+      (WidgetTester tester) async {
+    final weekday = DateTime.now();
+    final List<Transaction> transactions = [];
+
+    for (int i = 0; i < 7; i++) {
+      transactions.add(Transaction(
+        id: 't$i',
+        title: 'New Shoes',
+        amount: 69.99,
+        date: weekday.subtract(Duration(days: i)),
+      ));
+    }
+
+    final ChartController chartController = ChartController(
+      recentTransactions: transactions,
+    );
+
+    expect(chartController.recentTransactions.length, 7);
+
+    // every day should have 1 transaction
+    expect(
+        chartController.recentTransactions
+            .where((element) => element.amount == 69.99)
+            .length,
+        7);
+  });
+
+  testWidgets(
+      'Check if transaction_controller_test groups transactions correctly single day',
+      (WidgetTester tester) async {
+    final weekday = DateTime.now();
+    final List<Transaction> transactions = [];
+
+    for (int i = 0; i < 7; i++) {
+      transactions.add(Transaction(
+        id: 't$i',
+        title: 'New Shoes',
+        amount: 69.99,
+        date: weekday,
+      ));
+    }
+
+    final ChartController chartController = ChartController(
+      recentTransactions: transactions,
+    );
+
+    expect(chartController.groupedTransactions.length, 7);
+
+    // since the weekday is the same, only one day has transactions
+    expect(
+        chartController.groupedTransactions
+            .where((element) => element.value != 0)
+            .length,
+        1);
+    expect(
+        chartController.groupedTransactions
+            .where((element) => element.value == 0)
+            .length,
+        6);
+  });
+}
