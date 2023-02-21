a5abc1461a2d272a98d1a29575a833fc48c4ae73
samuelm00 <sa.mitterrutzner@gmail.com>
2023-02-21 16:41:24 +0100

    add simple test for trnasaction form controller

---
+++ test/transaction/controllers/create_transaction_form_controller_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_expense_tracker/features/transaction/controllers/create_transaction_form_controller.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+abstract class OnSubmitMock {
+  void onSubmit(String title, double amount);
+}
+
+class OnSubmitMockImpl extends Mock implements OnSubmitMock {}
+
+void main() {
+  testWidgets(
+      "handleSubmit() should not be called controllers that have empty texts",
+      (widgetTester) async {
+    final onSubmitMock = OnSubmitMockImpl();
+
+    final controller =
+        CreateTransactionFormController(onSubmit: onSubmitMock.onSubmit);
+
+    verifyNever(onSubmitMock.onSubmit("", 0));
+  });
+}
