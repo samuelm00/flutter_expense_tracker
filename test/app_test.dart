3313377a07ef8e031d0f17f4246e147b4199e146
samuelm00 <sa.mitterrutzner@gmail.com>
2023-02-23 18:30:58 +0100

    fix tests

--- test/app_test.dart
+++ test/app_test.dart
@@ -6,12 +6,17 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter_expense_tracker/app/app.dart';
+import 'package:flutter_expense_tracker/config/get_it/get_it.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
+  setUp(() {
+    SharedPreferences.setMockInitialValues({});
+    setupDependencies();
+  });
   testWidgets('Simple dummy test to verify if app renders',
       (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
     expect(find.byType(MyApp), findsOneWidget);
