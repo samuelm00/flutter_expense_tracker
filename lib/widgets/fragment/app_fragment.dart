15e71fd2df097f544316be263af32cd10b33e68a
samuelm00 <sa.mitterrutzner@gmail.com>
2023-02-24 09:29:30 +0100

    add listeners for creation deletion and failure

--- lib/widgets/fragment/app_fragment.dart
+++ lib/widgets/fragment/app_fragment.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_expense_tracker/features/chart/widgets/chart.dart';
 import 'package:flutter_expense_tracker/features/transaction/bloc/transaction_bloc.dart';
 import 'package:flutter_expense_tracker/features/transaction/bloc/transaction_event.dart';
+import 'package:flutter_expense_tracker/features/transaction/bloc/transaction_listener_factory.dart';
 import 'package:flutter_expense_tracker/features/transaction/bloc/transaction_state.dart';
 import 'package:flutter_expense_tracker/features/transaction/widgets/transaction_list.dart';
 import "package:flutter_expense_tracker/features/transaction/widgets/create_transaction_fb.dart";
@@ -20,28 +21,31 @@ class AppFragment extends StatelessWidget {
         ..add(const TransactionFetchRecentEvent()),
       child: BlocBuilder<TransactionBloc, TransactionState>(
         builder: (context, state) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text("Expense Tracker"),
-            ),
-            body: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Chart(
-                    recentTransactions: state.recentTransactions,
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  TransactionList(
-                    transactions: state.transactions,
-                  ),
-                ],
+          return MultiBlocListener(
+            listeners: TransactionListenerFactory.createTransactionListeners(),
+            child: Scaffold(
+              appBar: AppBar(
+                title: const Text("Expense Tracker"),
               ),
+              body: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Chart(
+                      recentTransactions: state.recentTransactions,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    TransactionList(
+                      transactions: state.transactions,
+                    ),
+                  ],
+                ),
+              ),
+              floatingActionButton: CreateTransactionFB(),
             ),
-            floatingActionButton: CreateTransactionFB(),
           );
         },
       ),
