621b8cc46b49f62c94726f17d3b92f49156b9076
samuelm00 <sa.mitterrutzner@gmail.com>
2023-02-27 09:08:29 +0100

    add transaction repository provider

---
+++ mobile/lib/config/dependency_config.dart
@@ -0,0 +1,10 @@
+import 'package:http/http.dart' as http;
+import 'package:flutter_expense_tracker/repositories/transactino_http_repository.dart';
+import 'package:flutter_expense_tracker/repositories/transaction_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final transactionRepositoryProvider = Provider<TransactionRepository>((ref) {
+  return TransactionHttpRepository(
+    httpClient: http.Client(),
+  );
+});
